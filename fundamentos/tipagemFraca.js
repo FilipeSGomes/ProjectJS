b86fc5b39f1f9c0e4b8f3fbe8abed5b932e4df0b
Filipe Gomes <filipesan7@gmail.com>
2020-11-24 19:51:11 -0300

    tipagem fraca JS

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,16 @@
+let qualquer = 'legal'
+
+console.log(qualquer)
+console.log( typeof qualquer)
+
+
+qualquer = 3.1415146
+console.log(qualquer)
+console.log( typeof qualquer)
+
+// Evitar nomes genérico e siglas
+
+let valor = ''
+
+let numero = 1
+let pqp = false //pesquisa de questoes publicas
---
+++ fundamentos/variaveisEConstansts.js
@@ -0,0 +1,12 @@
+var a = 5
+let b = 4
+
+var a = 6
+
+b = 10
+
+console.log(a, b)
+
+const c ="tatu";
+
+console.log(a, b, c)
